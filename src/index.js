fc0cc4c452762991a6958b23e2cc49f99f6b73a2
Aleksandr Lobkov <das149848@gmail.com>
2022-02-22 01:48:53 +0400

    feat: create function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    let newarr = [];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            newarr.push(arr[i][j]);
+        }
+    }
+    return newarr;
 }
